3769d4580358fd22071f7f232899a367af2b4e1f
TheDomple <dominick.sermeno@gmail.com>
2017-05-09 14:32:21 -0700

    Added locks
    added more locks and more logic

--- src/RuntimeThr.java
+++ src/RuntimeThr.java
@@ -2,6 +2,8 @@ import java.math.BigInteger;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class RuntimeThr implements Callable <String>
 {
@@ -10,6 +12,8 @@ public class RuntimeThr implements Callable <String>
 		private BlockingQueue<String> returnQue = new LinkedBlockingQueue<String>(); 
 		private BigInteger newVal = BigInteger.ZERO;
 		private String newRequest = "";
+		private Lock  lock = new ReentrantLock();
+	// Declare the Global Values of the current values to be updated and passed from thread to thread
 		public static BigInteger curFib, curEven, CurOdd;
 		public static int curPrime, curRand; 
 		
@@ -32,47 +36,82 @@ public class RuntimeThr implements Callable <String>
 			{
 				case "nextFib":
 				{
-					t1 = new NetworkThread("nextFib");
+					lock.lock(); 
+					try{
+					// pass the global value and request type into the thread constructor
+					t1 = new NetworkThread("nextFib"); //take in newVal
 					newRequest = requestQue.take();
 					msg = (t1.call());
 					returnQue.put(msg);
+					newVal = newVal.add(BigInteger.ZERO);// this needs to be changed to account for updated value
+					}finally{
+						lock.unlock();
+					}
 				}
 				break;
 				case "nextPrime":
 				{
-					t1 = new NetworkThread("nextPrime");
+					lock.lock(); 
+					try
+					{
+					// pass the global value and request type into the thread constructor
+					t1 = new NetworkThread("nextPrime");//take in newVal
 					newRequest = requestQue.take();
 					msg = (t1.call());
 					returnQue.put(msg);
+					newVal = newVal.add(BigInteger.ZERO);// this needs to be changed to account for updated value
+					} finally{
+						lock.unlock();
+					}
 				}
 				break;
 				case "nextRand":
 				{
-					t1 = new NetworkThread("nextRand");
+					lock.lock(); 
+					try{
+					// pass the global value and request type into the thread constructor
+					t1 = new NetworkThread("nextRand");//take in newVal
 					newRequest = requestQue.take();
 					msg = (t1.call());
 					returnQue.put(msg);
+					newVal = newVal.add(BigInteger.ZERO);// this needs to be changed to account for updated value
+					}finally{
+						lock.unlock();
+					}
 				}
 				break;
 				case "nextEven":
 				{
-					t2 = new LocalThread("nextFib",null);
+					lock.lock(); 
+					try{
+					// pass the global value and request type into the thread constructor
+					t2 = new LocalThread("nextEven",null);//take in newVal
 					newRequest = requestQue.take();
 					msg = (t2.call());
 					returnQue.put(msg);
+					newVal = newVal.add(BigInteger.ZERO);// this needs to be changed to account for updated value
+				}finally{
+					lock.unlock();
+					}
 				}
 				break;
 				case "nextOdd":
 				{
-					t2 = new LocalThread("nextFib",null);
+					lock.lock(); 
+					try{
+					// pass the global value and request type into the thread constructor
+					t2 = new LocalThread("nextOdd",null);//take in newVal
 					newRequest = requestQue.take();
 					msg = (t2.call());
 					returnQue.put(msg);
+					newVal = newVal.add(BigInteger.ZERO);// this needs to be changed to account for updated value
+					}finally{
+						lock.unlock();
+					}
 				}
 				break;
 			
 			}
-			
 			
 			return returnQue.poll();
 		}
